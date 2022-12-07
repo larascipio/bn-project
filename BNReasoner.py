6ce822ddfc7fb1f26c9e140bd6cb847f7513896d
Lara Scipio <larascipio@Air-van-Lara.home>
2022-12-07 22:32:07 +0100

    added multiple functions

--- BNReasoner.py
+++ BNReasoner.py
@@ -17,13 +17,15 @@ class BNReasoner:
             self.bn = net
 
     def get_structure(self, bn):
-
         return bn.draw_structure()
 
-    def get_edges(self):
-        return self.bn.structure.edges
+    def get_edges(self, bn):
+        return bn.structure.edges
 
     def pruning(self, variables, evidence):
+        """
+        Node- and edge-prune the Bayesian network given a set of query variables Q and evidence e.
+        """
         self.pruned_bn = copy.deepcopy(self.bn)
         self.variables = variables.union(evidence)
         self.evidence = evidence
@@ -43,16 +45,83 @@ class BNReasoner:
                     self.children = True 
             if not self.children:
                 break
-        
-        print(self.get_structure(self.pruned_bn))
         return self.pruned_bn
-        
     
-    # def d_separation(self, (x,y), evidence):
-    #     self.pruning()
+    def is_d_blocked(self, path, evidence):
+        """
+        Checks for every triple in a path if it is active, and subsequently if the path is d-blocked.
+        """
+        # Check every triple in the path 
+        for i in range(0, len(path)-2):
+            # Causal chain
+            if path[i+1] in self.bn.get_children(path[i]) and path[i+2] in self.bn.get_children(path[i+1]):
+                if path[i+1] in evidence:
+                    # Triple is inactive, path is d_blocked
+                    return True
+            
+            # Common cause 
+            if path[i] and path[i+2] in self.bn.get_children(path[i+1]):
+                if path[i+1] in evidence:
+                    # Triple is inactive, path is d_blocked
+                    return True
 
+            # Common effect
+            if path[i] and path[i+2] in self.bn.get_parents(path[i+1]):
+                if path[i+1] or self.bn.get_children(path[i+1]) not in evidence:
+                    # Triple is inactive, path is d-blocked
+                    return True
         
+        # If no inactive triples are found: path is not d-blocked
+        return False
+
+    def find_all_paths(self, bn, start, path):
+        """
+        Finds all possible (undirected) paths between a start and end node, 
+        returns a list of paths.
+        """
+        path.append(start)
+        # Depth first recursive search 
+        for node in bn.get_children(start) + bn.get_parents(start):
+            if node not in path:
+                self.find_all_paths(bn, node, path.copy())
+        paths.append(path)
+
+    def is_d_separated(self, X, Y, evidence):
+        """
+        Determines given three sets of variables X, Y, and Z, whether X is d-separated of Y given Z. 
+        """
+        for node in X:
+            self.find_all_paths(self.bn, node, [])
         
+        # Select all paths that end with a node in Y
+        selected_paths = []
+        for path in paths:
+            if not path:
+                continue
+            if path[-1:][0] in Y:
+                selected_paths.append(path)
+        
+        # Check for each path if d_blocked
+        for path in selected_paths:
+            if not self.is_d_blocked(path, evidence):
+                # Active path is not d-separated
+                print(f'{X} is not d-separated from {Y} given {evidence}')
+                return False
+        # If no paths are active: d-separated
+        print(f'{X} is d-separated from {Y} given {evidence}')
+        return True
+
+    def is_independent(self, X, Y, evidence):
+        """
+        Determines given three sets of variables X, Y, and Z, whether X is independent of Y given Z. 
+        """
+        if self.is_d_separated(X, Y, evidence):
+            print(f'{X} is independent from {Y} given {evidence}')
+            return True
+        print(f'{X} is not independent from {Y} given {evidence}')
+        return False
+        
+
 
 
 if __name__ == "__main__":
@@ -60,10 +129,14 @@ if __name__ == "__main__":
     test_file = 'testing/lecture_example.BIFXML'
     BN = BNReasoner(test_file)
     # BN.get_structure()
-    variables = {'Slippery Road?', 'Wet Grass?'}
-    evidence = {'Rain?'}
-    BN.pruning(variables, evidence)
-    # BN.d_separation(('Slippery Road?', 'Wet Grass?'), 'Rain')
+    X = {'Slippery Road?', 'Wet Grass?'}
+    Y = {'Winter?'}
+    evidence = {}
+    # BN.pruning(variables, evidence)
+    paths = [[]]
+    BN.is_d_separated(X, Y, evidence)
+    BN.is_independent(X, Y, evidence)
+
     # print(BN.get_edges())
     # #visualize
     # nx.draw(int_graph)
