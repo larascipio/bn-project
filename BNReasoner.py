4a6411f491e14fa987da81ca7f9319bfd890764c
Lara Scipio <larascipio@Air-van-Lara.home>
2022-12-07 11:11:30 +0100

    pruning changed

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,5 +1,6 @@
 from typing import Union
 from BayesNet import BayesNet
+import copy
 
 
 class BNReasoner:
@@ -15,35 +16,44 @@ class BNReasoner:
         else:
             self.bn = net
 
-    def get_structure(self):
-        print(self.bn.draw_structure())
+    def get_structure(self, bn):
+
+        return bn.draw_structure()
+
+    def get_edges(self):
+        return self.bn.structure.edges
 
     def pruning(self, variables, evidence):
+        self.pruned_bn = copy.deepcopy(self.bn)
         self.variables = variables.union(evidence)
         self.evidence = evidence
-        self.perform_once = False
-        self.children = False 
 
+        # Step 1: delete outgoing edges
+        for v in self.evidence:
+            children = self.pruned_bn.get_children(v)
+            for c in children:
+                self.pruned_bn.del_edge((v, c))
+        
         while True:
-            # Step 1: del leaf nodes 
-            for v in self.bn.get_all_variables():
-                if not self.bn.get_children(v) and v not in self.variables:
-                    self.children = True 
-                    self.bn.del_var(v)
-            if not self.children and self.perform_once:
-                break
-            
             self.children = False
-            self.perform_once = True 
-            # Step 2: del outgoing edges
-            for v in self.evidence:
-                children = self.bn.get_children(v)
-                for c in children:
-                    self.bn.del_edge((v, c))
+            # Step 2: delete leaf nodes recursively
+            for v in self.pruned_bn.get_all_variables():
+                if not self.pruned_bn.get_children(v) and v not in self.variables:
+                    self.pruned_bn.del_var(v)
+                    self.children = True 
+            if not self.children:
+                break
+        
+        print(self.get_structure(self.pruned_bn))
+        return self.pruned_bn
+        
+    
+    # def d_separation(self, (x,y), evidence):
+    #     self.pruning()
 
-        self.get_structure()
-                
-            
+        
+        
+
 
 if __name__ == "__main__":
     # Create test 
@@ -53,3 +63,8 @@ if __name__ == "__main__":
     variables = {'Slippery Road?', 'Wet Grass?'}
     evidence = {'Rain?'}
     BN.pruning(variables, evidence)
+    # BN.d_separation(('Slippery Road?', 'Wet Grass?'), 'Rain')
+    # print(BN.get_edges())
+    # #visualize
+    # nx.draw(int_graph)
+    # plt.show()
